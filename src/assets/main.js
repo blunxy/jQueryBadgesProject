b9abc0f4542d32306bd3c370b8e39afdb9a34781
$jpratt <$jpratt@mtroyal.ca>
2017-01-10 15:07:49 -0700

    init commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,28 @@
-$(function() {
+function addBadgeToPage(badge) {
+    var div = $('<div>', {
+        'class': 'course'
+    });
+    var title = $('<h3>').text(badge.title);
+    var image = $('<img>').attr('src', badge.badge);
+    var link = $('<a>', {
+        'href': badge.url
+    });
+    div.append(title, image, link);
+    $('#badges').append(div);
+}
 
-  // your code will go here
+$(function () {
+
+    $.ajax({
+        url: 'https://www.codeschool.com/users/blunxy.json',
+        dataType: 'jsonp',
+        success: function (resp) {
+            var courses = resp.courses.completed;
+            $.each(courses, function (index, course) {
+                addBadgeToPage(course);
+            })
+
+        }
+    })
 
 });
